3d67d45be668fa70fa1e0a2b5d0e16c1c1614b16
Addiryan <adrian@ribe.no>
2021-01-05 20:33:33 +0100

    add new styles

---
+++ src/styles/AppStyle.ts
@@ -0,0 +1,14 @@
+export const genericHeader:React.CSSProperties = {
+    padding: "60px",
+    textAlign: "center",
+    background: "linear-gradient(217deg, rgba(0,0,0,.8), rgba(2,0,0,0) 100.71%),#1abc9c",
+    color: "white",
+    fontSize: "30px"
+}
+
+export const mainArea:React.CSSProperties = {
+    paddingTop:"2rem",
+    background:"linear-gradient(to right,#2c5364, #203a43, #0f2027)",
+    textAlign:"center",
+    color:"white"
+}
--- src/styles/TextStyles.ts
+++ src/styles/TextStyles.ts
@@ -1,5 +1,5 @@
 
-export const formLabel = {
+export const formLabel:React.CSSProperties = {
     fontFamily: 'Roboto, sans-serif',
     fontSize: "1.2rem",
     marginLeft: "2rem",
@@ -11,12 +11,12 @@ export const formLabel = {
 
 export const inputNickname:React.CSSProperties = {
     fontFamily: 'Roboto, sans-serif',
-    color:"#333",
+    color:"black",
     fontSize:"1.2rem",
     margin:"0 auto",
     padding:"1.5rem 2rem",
     borderRadius: "0.2rem",
-    backgroundColor:"rgb(255, 255, 255)",
+    // backgroundColor:"rgb(255, 255, 255)",
     border:"none",
     width:"20%",
     display:"block",
@@ -26,30 +26,26 @@ export const inputNickname:React.CSSProperties = {
 
 export const inputMessage:React.CSSProperties = {
     fontFamily: 'Roboto, sans-serif',
-    color:"#333",
     fontSize:"1.2rem",
-    margin:"0px auto",
     padding:"1.5rem 2rem",
     borderRadius: "0.2rem",
-    backgroundColor:"rgb(255, 255, 255)",
     alignContent:"start",
     textAlign:"start",
     border:"none",
     width:"20%",
-    height:"40%",
+    height:"60%",
+    color:"black",
     display:"block",
     borderBottom:"0.3rem solid transparent",
     transition:"all 0.3s"
 }
 
 export const inputArea:React.CSSProperties = {
-    height: "25vh",
+    height: "10vh",
 	width: "100%",
-    // background: "linear-gradient(to right,#2c5364, #203a43, #0f2027)",
-    background: "#1abc9c",
     display: "flex",
     flexDirection: "column",
-    justifyContent: "space-around",
+    justifyContent: "space-between",
     alignItems: "center",
-    color: "#fff"
+    color: "black"
 }
